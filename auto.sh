e0630a8cbb5c5f4a7be456fafa6179db60a577ef
mzpbvsig <972403325@qq.com>
2017-06-09 11:50:14 +0800

    lib copy 和 function.py优化

--- auto.sh
+++ auto.sh
@@ -4,10 +4,10 @@ while read -r line; do
   IFS=' ' read -r -a array <<< "$line"
   name="${array[0]}.apk"
   durl=${array[1]}
-  if [ ! -f $s_dir ]; then
+  if [ ! -f $name ]; then
     curl -o "${name}" $durl
   fi
   ./sdkauto.sh "${name}" "sdk.apk"
 done <<< "$data"
 
-ls
+ls -t
